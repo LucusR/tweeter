e0a9a2d4e673b3561efd23566b95d5e4545ecaf3
Lucus Riccioni <lucusroni@gmail.com>
2022-04-04 03:30:17 +0000

    Functional Character Counter for tweet text box

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,15 +1,18 @@
 $(document).ready(function() {
   
 
-
-
-
-
-
-
-
-
-
+$("#tweet-form").on("keyup", "textarea", function() {
+  $input = $(this).val()
+  $charactersRemaining = 140 - $input.length
   
+  $counter = $(".counter")
+  $counter.text($charactersRemaining)
+
+  if ($charactersRemaining < 0) {
+    $counter.addClass("redFont")
+  } else {
+    $counter.removeClass("redFont")
+    } 
+});
 });
 

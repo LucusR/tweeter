9e7dcd9188d9be4480f479a1cfc728afb325e373
Lucus Riccioni <lucusroni@gmail.com>
2022-04-05 01:41:24 +0000

    Eslint

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,18 +1,18 @@
 $(document).ready(function() {
   
 
-$("#tweet-form").on("keyup", "textarea", function() {
-  $input = $(this).val()
-  $charactersRemaining = 140 - $input.length
+  $("#tweet-form").on("keyup", "textarea", function() {
+    $input = $(this).val();
+    $charactersRemaining = 140 - $input.length;
   
-  $counter = $(".counter")
-  $counter.text($charactersRemaining)
+    $counter = $(".counter");
+    $counter.text($charactersRemaining);
 
-  if ($charactersRemaining < 0) {
-    $counter.addClass("redFont")
-  } else {
-    $counter.removeClass("redFont")
-    } 
-});
+    if ($charactersRemaining < 0) {
+      $counter.addClass("redFont");
+    } else {
+      $counter.removeClass("redFont");
+    }
+  });
 });
 
